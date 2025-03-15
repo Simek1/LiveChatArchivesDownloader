d364b2c94304d172785e5fac5d7182ccc45f0c67
Simek1 <t.borowiak98@gmail.com>
2025-03-15 04:42:02 +0100

    Added pagination

--- request.py
+++ request.py
@@ -2,24 +2,27 @@ import requests
 
 
 
-def list_archives(token, authorization_type = "Basic"):
+def list_archives(token, authorization_type = "Basic", page_id = ""):
     url = "https://api.livechatinc.com/v3.5/agent/action/list_archives"
 
     headers = {
         "Authorization": f"{authorization_type} {token}",
         "Content-Type": "application/json"
     }
-    data = {
-        "limit": 100,
-        "filters": {
-            "event_types": {
-                "values": ["message", "filled_form"]
+    if page_id == "":
+        data = {
+            "limit": 100,
+            "filters": {
+                "event_types": {
+                    "values": ["message", "filled_form"]
+                }
             }
         }
-    }
+    else: 
+        data = {
+            "page_id": page_id
+        }
     response = requests.post(url, headers=headers, json=data)
     print(response.status_code)
-    print(response.json())
-
 
 
