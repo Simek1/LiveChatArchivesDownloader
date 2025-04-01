1142e292a0c3f648f41d5d04018d709062e43621
Simek1 <74107454+Simek1@users.noreply.github.com>
2025-04-01 04:24:22 +0200

    Message about request status in GUI

--- export_archive.py
+++ export_archive.py
@@ -18,5 +18,6 @@ def export_archive(token, token_type = "Basic", file_name = "exported_archives.j
             else:
                 print(f"API call has returned {response[0]} status")
                 exporting = False
+            return response[0] #status is returned to display status of request in gui
 
 #if you're using the PAT authorization, then you need only to provide your token to the function, e.g. export_archive(your_token)
--- gui.py
+++ gui.py
@@ -11,6 +11,7 @@ class DownloaderWindow(wx.Frame):
         self.toke_type_label = wx.StaticText(panel, pos=(2, 62), label = "Token type:")
         self.token_types = ["Basic", "Bearer"]
         self.token_type_choice = wx.Choice(panel, pos=(75, 60), size=(200,25), choices=self.token_types)
+        self.status_label = wx.StaticText(panel, pos=(0, 145), label="")
 
         self.export_button.Bind(wx.EVT_BUTTON, self.on_export_button_click)
         
@@ -20,8 +21,20 @@ class DownloaderWindow(wx.Frame):
     def on_export_button_click(self, event):
         token = self.token_input.GetValue()
         token_type = self.token_type_choice.GetStringSelection()
-        export_archive(token, token_type=token_type)
-
+        status = export_archive(token, token_type=token_type)
+        if status == 200:
+            self.status_label.SetLabel("Archive has been exported successfully")
+            self.status_label.SetForegroundColour((225, 225, 225))
+        else:
+            self.status_label.SetLabel(f"Request has failed with status {status}")
+            self.status_label.SetForegroundColour((225, 0, 0))
+        status_label_size = self.status_label.GetSize()
+        status_label_old_pos = self.status_label.GetPosition()
+        if status_label_size[0] > 300:
+            self.status_label.SetPosition((0, status_label_old_pos[1]))
+        else:
+            new_x_pos = int((300 - status_label_size[0]) / 2)
+            self.status_label.SetPosition((new_x_pos, status_label_old_pos[1]))
 
 if __name__ == "__main__":
     app = wx.App(False)
