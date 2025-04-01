75549c3267125a8792b75f901431e8ef61ecc581
Simek1 <74107454+Simek1@users.noreply.github.com>
2025-04-01 03:43:45 +0200

    Added token type selection

--- gui.py
+++ gui.py
@@ -5,8 +5,12 @@ class DownloaderWindow(wx.Frame):
     def __init__(self):
         super().__init__(parent = None, title="LiveChat Archives Downloader", size=(300,200))
         panel = wx.Panel(self)
-        self.token_input = wx.TextCtrl(panel, pos=(50,30), size=(200,25))
-        self.export_button = wx.Button(panel, pos=(95,80), label="Export Archive")
+        self.token_label = wx.StaticText(panel, pos=(2, 32), label = "Token:")
+        self.token_input = wx.TextCtrl(panel, pos=(75,30), size=(200,25))
+        self.export_button = wx.Button(panel, pos=(95,115), label="Export Archive")
+        self.toke_type_label = wx.StaticText(panel, pos=(2, 62), label = "Token type:")
+        self.token_types = ["Basic", "Bearer"]
+        self.token_type_choice = wx.Choice(panel, pos=(75, 60), size=(200,25), choices=self.token_types)
 
         self.export_button.Bind(wx.EVT_BUTTON, self.on_export_button_click)
         
@@ -15,7 +19,8 @@ class DownloaderWindow(wx.Frame):
 
     def on_export_button_click(self, event):
         token = self.token_input.GetValue()
-        export_archive(token)
+        token_type = self.token_type_choice.GetStringSelection()
+        export_archive(token, token_type=token_type)
 
 
 if __name__ == "__main__":
